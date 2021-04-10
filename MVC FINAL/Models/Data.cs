727b0fcca8d85e30a500cb5590d6a8aaa615df25
ChintanPatel-9 <77534156+ChintanPatel-9@users.noreply.github.com>
2021-04-10 23:03:15 +0530

    Add files via upload

---
+++ MVC FINAL/Models/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotesMarketplace.Models
+{
+    public class Data
+    {
+        public User user { get; set; }
+
+        public int P_ID { get; set; }
+        public String name { get; set; }
+    }
+}
